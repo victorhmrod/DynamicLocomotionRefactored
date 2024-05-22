6e6bcafc8dfe73636e0c329512b2a8679828c39c
victorhmrod <vhcgames1@gmail.com>
2024-05-22 13:23:27 -0300

    Initial commit

---
+++ Source/DLExtras/Private/DLExtras.cpp
@@ -0,0 +1,17 @@
+﻿#include "DLExtras.h"
+
+#define LOCTEXT_NAMESPACE "FDLExtrasModule"
+
+void FDLExtrasModule::StartupModule()
+{
+    
+}
+
+void FDLExtrasModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FDLExtrasModule, DLExtras)
