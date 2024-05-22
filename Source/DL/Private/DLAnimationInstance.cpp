64f0e94043c5b214e48822d8a5aed405aa1dd830
victorhmrod <vhcgames1@gmail.com>
2024-05-22 18:20:45 -0300

    Animmation instance base

--- Source/DL/Private/DLAnimationInstance.cpp
+++ Source/DL/Private/DLAnimationInstance.cpp
@@ -2,3 +2,43 @@
 
 
 #include "DLAnimationInstance.h"
+
+#include "DLCharacter.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+void UDLAnimationInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	Character = Cast<ADLCharacter>(TryGetPawnOwner());
+}
+
+void UDLAnimationInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	RefreshLocomotion();
+}
+
+void UDLAnimationInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	//RefreshLocomotion();
+}
+
+void UDLAnimationInstance::RefreshLocomotion()
+{
+	if (!TryGetPawnOwner()) return;
+	if (!IsValid(Character)) return;
+
+	bIsFalling = TryGetPawnOwner()->GetMovementComponent()->IsFalling();
+		
+	RotationRate = Character->CurrentSpeed;
+	Speed = Character->CurrentRotationRate;
+	bIsMoving = Character->bIsMoving;
+	bIsAccelerating = Character->bIsAccelerating;
+	SpeedWhenStopping = Character->SpeedWhenStopping;
+	LateralSpeed = Character->LateralSpeed;
+	SpeedRequiredForLeap = Character->SpeedRequiredForLeap;
+}
--- Source/DL/Public/DLAnimationInstance.h
+++ Source/DL/Public/DLAnimationInstance.h
@@ -13,4 +13,59 @@ UCLASS()
 class DL_API UDLAnimationInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+#pragma region Unreal Defaults
+	
+private:
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+#pragma endregion 
+
+#pragma region Blueprint Exposed
+
+protected:
+	
+	UPROPERTY(BlueprintReadWrite)
+	TObjectPtr<class ADLCharacter> Character;
+	
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsFalling{false};
+
+	UPROPERTY(BlueprintReadWrite)
+	float Speed{0};
+	
+	UPROPERTY(BlueprintReadWrite)
+	float RotationRate{0};
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsMoving{false};
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsAccelerating{false};
+	
+	UPROPERTY(BlueprintReadWrite)
+	float SpeedWhenStopping{0};
+
+	UPROPERTY(BlueprintReadWrite)
+	float LateralSpeed{0};
+	
+	UPROPERTY(BlueprintReadWrite)
+	bool StopL{false};
+	
+	UPROPERTY(BlueprintReadWrite)
+	bool IsLandingWhileMobile{false};
+
+	UPROPERTY(BlueprintReadWrite)
+	float SpeedRequiredForLeap{800};
+
+#pragma endregion 
+
+#pragma region CPP Only
+	
+private:
+	void RefreshLocomotion();
+
+#pragma endregion 
 };
