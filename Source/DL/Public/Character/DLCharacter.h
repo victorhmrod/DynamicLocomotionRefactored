e9aa4f33fd51119b4345d27a0a477bc3b0a3b333
victorhmrod <vhcgames1@gmail.com>
2024-05-22 15:11:03 -0300

    Started C++ development

---
+++ Source/DL/Public/Character/DLCharacter.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "DLCharacter.generated.h"
+
+UCLASS()
+class DL_API ADLCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+#pragma region Unreal Defaults
+	
+public:
+	ADLCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+#pragma endregion
+
+#pragma region Blueprint Exposed
+	
+	UPROPERTY(BlueprintReadOnly)
+	float Delta{0};
+	
+	UPROPERTY(BlueprintReadOnly)
+	float CurrentRotationRate{0};
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsMoving{false};
+
+	UPROPERTY(BlueprintReadOnly)
+	float CurrentSpeed{0};
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsAccelerating{false};
+
+	UPROPERTY(BlueprintReadOnly)
+	float SpeedWhenStopping{0};
+
+	UPROPERTY(BlueprintReadOnly)
+	float LateralSpeed{0};
+	
+	UPROPERTY(BlueprintReadWrite)
+	float MaxSpeedTarget{500.f};
+
+#pragma endregion
+
+#pragma region CPP Only
+
+	void RefreshLocomotion(float DeltaTime);
+
+	bool bLocked{false};
+
+#pragma endregion
+};
